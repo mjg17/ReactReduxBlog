069a5da6041fee01bbf6c168ae29fad7bbc36edc
Michael Gray <michael@michaelgray.info>
2017-10-23 16:42:35 +0100

    To 9-135

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-    	<h3>New Post</h3>
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
