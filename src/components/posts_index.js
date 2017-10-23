d08a81dbf5aae9a2d1bebc2b46cf40921fa98345
Michael Gray <michael@michaelgray.info>
2017-10-23 15:54:27 +0100

    To 9-133

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchPosts } from '../actions';
 
@@ -8,15 +10,37 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+	  {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-    	Posts Index
+	<div className="text-xs-right">
+	  <Link className="btn btn-primary" to="/posts/new">
+	    Add a post
+	  </Link>
+	</div>
+    	<h3>Posts</h3>
+	<ul className="list-group">
+	    {this.renderPosts()}
+	</ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
 //                           ^^^^^^^^^^^^^^
 // is short-hand equivalent to the mapDispatchToProps() functions we've been writing
--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex} />
-      </div>
+      <Switch>
+        <Route exact path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
+      </Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
